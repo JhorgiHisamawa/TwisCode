e7dc02081ac94453e6242b9c8edc8e4c9df3e1fe
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-09-01 11:05:17 +0700

    Update no2.sql

--- no2.sql
+++ no2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE detail_transaksi (id serial NOT NULL, id_transaksi serial NOT NULL, Harga INT NOT NULL, Jumlah INT NOT NULL, Subtotal INT CONSTRAINT fk_transaksi FOREIGN KEY (id)  REFERENCES transaksi(id));
+CREATE TABLE detail_transaksi (id serial NOT NULL, id_transaksi serial NOT NULL, Harga INT NOT NULL, Jumlah INT NOT NULL, Subtotal INT NOT NULL, CONSTRAINT fk_transaksi FOREIGN KEY (id)  REFERENCES transaksi(id));
 
 --insert default value (dummy)
 INSERT INTO detail_transaksi(id, Harga, Jumlah, Subtotal) VALUES(1, 5000, 5, 25000);
